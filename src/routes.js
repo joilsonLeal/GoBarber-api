65fcf57db7742787b9584d2bca45f80541e4816f
= <=>
2020-07-16 18:42:37 -0300

    configuração do nodemon, sucrase e debugger

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res)  => {
-    return res.send('oi');
+    return res.json({message: 'Hello teste'});
 });
 
-module.exports = routes;
+export default routes;
