8e18c8c294fee25ae4a9e29689fa86debadd5210
= <=>
2020-07-17 13:54:52 -0300

    configuração do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    dialect: 'sqlite',
+    username: '',
+    password: '',
+    database: 'gobarber',
+    storage: './database/gobarber.sqlite',
+    define: {
+        underscored: true,
+        timestamp: true,
+        underscoredAll: true,
+    },
+};
