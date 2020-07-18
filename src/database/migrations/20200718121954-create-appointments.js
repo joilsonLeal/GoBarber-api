9d5b415422ee32f43520532b360b0cbc7cc113b9
= <=>
2020-07-18 09:28:23 -0300

    adição de migration de appointments

---
+++ src/database/migrations/20200718121954-create-appointments.js
@@ -0,0 +1,46 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('appointments', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            date: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                references: { model: 'users', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+            },
+            provider_id: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                references: { model: 'users', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+            },
+            canceled_at: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('appointments');
+    },
+};
