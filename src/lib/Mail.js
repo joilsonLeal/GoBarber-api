3e558bed8cd145b6f3bdbcf159da2fa4de170a90
= <=>
2020-07-19 16:11:28 -0300

    implementação de template de e-mail

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,8 @@
 import nodemailer from 'nodemailer';
-import mailconfig from '../config/mail'
+import { resolve } from 'path';
+import mailconfig from '../config/mail';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 
 class Mail {
     constructor() {
@@ -11,6 +14,23 @@ class Mail {
             secure,
             auth: auth.user ? auth : null,
         });
+
+        this.configureTemplates();
+    }
+
+    configureTemplates() {
+        const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+        this.transporter.use('compile', nodemailerhbs({
+            viewEngine: exphbs.create({
+                layoutsDir: resolve(viewPath, 'layouts'),
+                partialsDir: resolve(viewPath, 'partials'),
+                defaultLayout: 'default',
+                extname: '.hbs',
+            }),
+            viewPath,
+            extName: '.hbs',
+        }));
     }
 
     sendMail(message) {
